aca24e0af77ee7edf2becbf867f3a3298879ebd7
github username <caseyoksana@gmail.com>
2020-12-14 22:00:01 -0500

    Added User show aaction

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # GET /api/v1/users/:id
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:show]
+    end
+  end
 end
