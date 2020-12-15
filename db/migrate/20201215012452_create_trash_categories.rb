d8b34fc93f4bba1305ef49ff40a37d5053b72c7c
github username <caseyoksana@gmail.com>
2020-12-14 21:27:36 -0500

    Faker Seed Data

--- db/migrate/20201215012452_create_trash_categories.rb
+++ db/migrate/20201215012452_create_trash_categories.rb
@@ -1,7 +1,7 @@
 class CreateTrashCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :trash_categories do |t|
-      t.string :type
+      t.string :kind
       t.string :image
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleting Old recods"
+TrashCategory.destroy_all
+User.destroy_all
+
+puts "Creating new records!"
+TrashCategory.create!(
+    kind: "landfill",
+    image: "https://dur-cjweb.newscyclecloud.com/storyimage/CJ/20200904/NEWS01/200909926/AR/0/AR-200909926.jpg"
+)
+
+TrashCategory.create!(
+    kind: "compost",
+    image: "https://cdn.shopify.com/s/files/1/0272/1982/0604/articles/composting-image.jpg?v=1572870863"
+)
+
+TrashCategory.create!(
+    kind: "recycling",
+    image: "https://hips.hearstapps.com/hmg-prod/images/plastic-recycling-symbols-1602875439.jpg"
+)
+
+5.times do
+    user = User.create!(
+        name: Faker::Movies::LordOfTheRings.character,
+        email: "lotr@gmail.com",
+        password: 12345
+    )
+end
+
+puts "Done!"
+
