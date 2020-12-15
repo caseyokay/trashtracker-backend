ee0171f17d4442d7d6e62b4282cfff077f8b3d5c
github username <caseyoksana@gmail.com>
2020-12-14 21:41:36 -0500

    Seeded more data & checked that the join table works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Deleting Old recods"
-TrashCategory.destroy_all
+puts "🔫 Deleting Old records"
+# TrashCategory.destroy_all
 User.destroy_all
+TrashItem.destroy_all
 
-puts "Creating new records!"
+
+puts "🧝‍♀️ Creating new records!"
 TrashCategory.create!(
     kind: "landfill",
     image: "https://dur-cjweb.newscyclecloud.com/storyimage/CJ/20200904/NEWS01/200909926/AR/0/AR-200909926.jpg"
@@ -20,18 +22,33 @@ TrashCategory.create!(
     image: "https://cdn.shopify.com/s/files/1/0272/1982/0604/articles/composting-image.jpg?v=1572870863"
 )
 
-TrashCategory.create!(
+trash = TrashCategory.create!(
     kind: "recycling",
     image: "https://hips.hearstapps.com/hmg-prod/images/plastic-recycling-symbols-1602875439.jpg"
 )
 
 5.times do
-    user = User.create!(
+    User.create!(
         name: Faker::Movies::LordOfTheRings.character,
         email: "lotr@gmail.com",
         password: 12345
     )
 end
 
-puts "Done!"
+user = User.create!(
+    name: Faker::Movies::LordOfTheRings.character,
+    email: "lotr@gmail.com",
+    password: 12345
+)
+
+3.times do
+    TrashItem.create!(
+        description: Faker::TvShows::Buffy.quote,
+        date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+        user: user,
+        trash_category: trash
+    )
+end
+
+puts "🔮 Done!"
 
