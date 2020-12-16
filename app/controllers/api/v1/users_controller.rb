3ecc496a2fdd30196893e2db90c7d9027e4fca4d
github username <caseyoksana@gmail.com>
2020-12-15 23:10:29 -0500

    routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::UsersController < ApplicationController
     user = User.find(params[:id])
     render json: user 
   end
+
+  def index
+    users = User.all 
+    render json: users 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # GET /api/v1/users/:id
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:show]
+      resources :users, only: [:show, :index]
     end
   end
 end
