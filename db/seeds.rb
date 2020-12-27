58bb2f9e968baeaad58840f7b868cc59823cff86
github username <caseyoksana@gmail.com>
2020-12-26 21:11:52 -0500

    new seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,36 +14,40 @@ TrashCategory.destroy_all
 puts "🧝‍♀️ Creating new records!"
 TrashCategory.create!(
     kind: "landfill",
-    image: "https://dur-cjweb.newscyclecloud.com/storyimage/CJ/20200904/NEWS01/200909926/AR/0/AR-200909926.jpg"
+    image: "https://i.ibb.co/dKM699P/Screen-Shot-2020-12-26-at-8-49-08-PM.png"
 )
+#"https://dur-cjweb.newscyclecloud.com/storyimage/CJ/20200904/NEWS01/200909926/AR/0/AR-200909926.jpg"
 
 TrashCategory.create!(
     kind: "compost",
-    image: "https://cdn.shopify.com/s/files/1/0272/1982/0604/articles/composting-image.jpg?v=1572870863"
+    image: "https://i.ibb.co/kDH0cL3/Screen-Shot-2020-12-26-at-8-45-59-PM.png"
 )
+#"https://cdn.shopify.com/s/files/1/0272/1982/0604/articles/composting-image.jpg?v=1572870863"
 
 trash = TrashCategory.create!(
     kind: "recycling",
-    image: "https://www.coralville.org/ImageRepository/Document?documentID=9113"
+    image: "https://i.ibb.co/7pyKWMX/Screen-Shot-2020-12-26-at-8-41-32-PM.png"
 )
+
+# "https://www.coralville.org/ImageRepository/Document?documentID=9113"
 
 5.times do
     User.create!(
-        name: Faker::Movies::LordOfTheRings.character,
+        name: Faker::TvShows::Buffy.character,
         email: "lotr@gmail.com",
         password: 12345
     )
 end
 
 user = User.create!(
-    name: "Galadriel",
+    name: "Casey",
     email: "lotr@gmail.com",
     password: 12345
 )
 
 3.times do
     TrashItem.create!(
-        description: Faker::TvShows::Buffy.quote,
+        description: Faker::Food.dish,
         date: Faker::Date.between(from: 2.days.ago, to: Date.today),
         user: user,
         trash_category: trash
